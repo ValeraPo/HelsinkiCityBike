1b49870bd2a5ebcc52c7d8ab69d25d9eff01468f
Valera <portnovavalerija@mail.ru>
2022-12-08 13:00:02 +0200

    Add project files.

---
+++ HelsinkiCityBike.API/Controllers/JourneyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelsinkiCityBike.API.Controllers
+{
+    [ApiController]
+    [Route("api/journey")]
+    public class JourneyController : Controller
+    {
+        // api/journey
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetAllJourneys()
+        {
+            return Ok();
+        }
+
+        // api/journey(42)
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetJourneyById(int id)
+        {
+            return Ok($"{id}");
+        }
+
+    }
+}
---
+++ HelsinkiCityBike.API/Controllers/StationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelsinkiCityBike.API.Controllers
+{
+    [ApiController]
+    [Route("api/station")]
+    public class StationController : Controller
+    {
+        // api/station
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetAllStations()
+        {
+            return Ok();
+        }
+
+        // api/station(42)
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> GetStationById(int id)
+        {
+            return Ok($"{id}");
+        }
+
+    }
+}
