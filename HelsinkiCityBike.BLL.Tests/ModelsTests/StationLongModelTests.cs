9348d1186ba6eef7137de8635b2ddbac8a241b12
Valera <portnovavalerija@mail.ru>
2022-12-17 23:54:32 +0200

    Add tests for models

---
+++ HelsinkiCityBike.BLL.Tests/ModelsTests/StationLongModelTests.cs
@@ -0,0 +1,46 @@
+﻿using HelsinkiCityBike.BLL.Models;
+using NUnit.Framework;
+
+namespace HelsinkiCityBike.BLL.Tests.ModelsTests
+{
+    public class StationLongModelTests
+    {
+        [TestCase(15000, 10, 1.5F)]
+        [TestCase(0, 24, 0)]
+        [TestCase(67867869.99F, 666, 101.9F)]
+        public void AvgDistanceOfJourneyStartingFrom_ShouldConvertCoveredAvgDistance(float avgDistance, int amountJourneys, float expected)
+        {
+            //given
+            var station = new StationLongModel
+            {
+                NumberOfJourneysStartingFrom = amountJourneys,
+                AvgDistanceOfJourneyStartingFrom = avgDistance
+            };
+
+            //when
+            var actual = station.AvgDistanceOfJourneyStartingFrom;
+
+            //then
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(15000, 10, 1.5F)]
+        [TestCase(0, 24, 0)]
+        [TestCase(67867869.99F, 666, 101.9F)]
+        public void AvgDistanceOfJourneyEndingAt_ShouldConvertCoveredAvgDistance(float avgDistance, int amountJourneys, float expected)
+        {
+            //given
+            var station = new StationLongModel 
+            {
+                NumberOfJourneysEndingAt = amountJourneys,
+                AvgDistanceOfJourneyEndingAt = avgDistance
+            };
+
+            //when
+            var actual = station.AvgDistanceOfJourneyEndingAt;
+
+            //then
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ HelsinkiCityBike.DAL.Tests/EntitiesTests/JourneyTests.cs
@@ -0,0 +1,38 @@
+﻿using HelsinkiCityBike.DAL.Entities;
+using NUnit.Framework;
+
+namespace HelsinkiCityBike.DAL.Tests
+{
+    public class JourneyTests
+    {
+        [TestCase(1500, 1.5F)]
+        [TestCase(0, 0)]
+        [TestCase(6789.99F, 6.79F)]
+        public void CoveredDistanceTest_ShouldConvertCoveredDistance(float distance, float expected)
+        {
+            //given
+            var journey = new Journey { CoveredDistance = distance };
+
+            //when
+            var actual = journey.CoveredDistance;
+
+            //then
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(60, 1)]
+        [TestCase(0, 0)]
+        [TestCase(876, 14.6F)]
+        public void DurationTest_ShouldConvertCoveredDuration(int duration, float expected)
+        {
+            //given
+            var journey = new Journey { Duration = duration };
+
+            //when
+            var actual = journey.Duration;
+
+            //then
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
